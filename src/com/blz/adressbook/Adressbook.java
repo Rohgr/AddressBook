2b6c8c437a0f7401344b5a21f2131814260c0773
rohgr <rdagrawal6@gmail.com>
2022-03-03 21:27:31 +0530

    Addin contacts for Addressbook

--- src/com/blz/adressbook/Adressbook.java
+++ src/com/blz/adressbook/Adressbook.java
@@ -1,7 +1,14 @@
 package com.blz.adressbook;
 
 public class Adressbook {
-
+    private String firstName;
+    private String lastname;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private int phoneNumber;
+    private String email;
      public static void main(String[] args) {
          System.out.println("Welcome to Adress book");
 
