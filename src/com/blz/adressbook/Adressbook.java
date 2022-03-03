7db8ab1b482ec1dd3ad6b326e7e7bb946576099b
rohgr <rdagrawal6@gmail.com>
2022-03-03 21:11:21 +0530

    Adding part 1 Addressbook program

---
+++ src/com/blz/adressbook/Adressbook.java
@@ -0,0 +1,11 @@
+package com.blz.adressbook;
+
+public class Adressbook {
+
+     public static void main(String[] args) {
+         System.out.println("Welcome to Adress book");
+
+    }
+
+
+}
---
+++ src/com/blz/adressbook/Main.java
@@ -0,0 +1,8 @@
+package com.blz.adressbook;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
